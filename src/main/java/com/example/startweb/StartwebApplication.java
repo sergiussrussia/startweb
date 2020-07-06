19d7a1bebdb04fb03a3782c91d6b68d112d8c02f
Administrator <sergiussrussia@gmail.com>
2020-07-06 18:32:43 +0300

    Initial commit

---
+++ src/main/java/com/example/startweb/StartwebApplication.java
@@ -0,0 +1,13 @@
+package com.example.startweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/startweb/StartwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.startweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
